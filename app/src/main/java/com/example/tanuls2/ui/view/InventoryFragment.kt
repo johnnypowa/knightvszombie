90f598bec02989473d6384caef51fd7b11eaffc1
DESKTOP-6H7KLPO\johnny <janos.szabo0125@gmail.com>
2022-05-02 22:43:35 +0200

    Refact the UI architecture of the app to single activity pattern + add navigation bar and setup navigation components + use fragments instead of activity to show the content of the app

---
+++ app/src/main/java/com/example/tanuls2/ui/view/InventoryFragment.kt
@@ -0,0 +1,20 @@
+package com.example.tanuls2.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tanuls2.R
+
+class InventoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_inventory, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
