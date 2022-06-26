2d20837d81f3fa6d055d903c509373408cd5ccfc
DESKTOP-6H7KLPO\johnny <janos.szabo0125@gmail.com>
2022-06-26 17:04:29 +0200

    Hide bottom navigation bar on item details fragment

--- app/src/main/java/com/example/tanuls2/ui/MainActivity.kt
+++ app/src/main/java/com/example/tanuls2/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tanuls2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,9 +23,12 @@ class MainActivity : AppCompatActivity() {
         val navigationView = findViewById<BottomNavigationView>(R.id.navView)
         navigationView.setupWithNavController(navController)
 
-        //navigationView.setOnNavigationItemReselectedListener {
-
-        //}
+        navController.addOnDestinationChangedListener { _, navDestination, _ ->
+            when (navDestination.id) {
+                R.id.combat, R.id.inventory, R.id.skills  -> navigationView.visibility = VISIBLE
+                R.id.itemDetails -> navigationView.visibility = GONE
+            }
+        }
 
     }
 }
